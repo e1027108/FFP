51c69eeae790cad6d51c0475b82f9d47878470c1
patrick.bellositz <patrick.bellositz@128.131.203.73>
2017-03-20 14:46:51 +0100

    updated type definitions/signatures

--- UE1/lu1.hs
+++ UE1/lu1.hs
@@ -1,11 +1,24 @@
+import Prelude hiding (sqrt,repeat)
+
+type InitialApprox = Double -- Auschliesslich Werte > 0
+type Epsilon = Double -- Auschliesslich Werte > 0
+type SquareArg = Double -- Auschliesslich Werte > 0
+type Approx = Double -- Auschliesslich Werte > 0
+type Map = Double -> Double
+type Low = Double -- Untere Intervallgrenze
+type High = Double -- Obere Intervallgrenze
+type Area = Double
+type XCoordinate = Double
+type InitialH = Double -- Auschliesslich Werte > 0
+type Slope = Double
+
 --Generator function - takes a function with 1 parameter,
 --creates a new function that takes the second parameter (normally masked by currying?)
 --so we can add the start value to the list.
 
---TODO: is this start value really necessary? for instance: sqrt 1 1 5 = [1,3,2.33,...].
 -- this influences the outcome.
-repeat' :: (a -> a) -> a -> [a]
-repeat' f x = (x:(repeat' f (f x)))
+repeat :: (a -> a) -> a -> [a]
+repeat f x = (x:(repeat f (f x)))
 
 --Selector functions - used to terminate the generator.
 within :: (Ord a, Num a) => a -> [a] -> a
@@ -19,11 +32,11 @@ relative eps (a:b:rest)
  | otherwise = relative eps (b:rest)
 
 --approximation strategy for an integral, very naive
-easyintegrate :: (Double -> Double) -> Double -> Double -> Double
+easyintegrate :: Map -> Low -> High -> Area
 easyintegrate f a b = (f a + f b) * (b-a)/2
 
 --improved variation of easyintegrate, better approximation
-integrateNaive :: (Double -> Double) -> Double -> Double -> [Double]
+integrateNaive :: Map -> Low -> High -> [Area]
 integrateNaive f a b = ((easyintegrate f a b):
                            (map addpair (zip (integrateNaive f a mid) 
                              (integrateNaive f mid b)))) 
@@ -32,7 +45,7 @@ integrateNaive f a b = ((easyintegrate f a b):
 --optimized version of integrateNaive, better performance
 --example: testF 0 2 0.000000000001; 
 --integrateNaive runs ~32s, integrateEfficient ~21s (on a laptop)
-integrateEfficient :: (Double -> Double) -> Double -> Double -> [Double]
+integrateEfficient :: Map -> Low -> High -> [Area]
 integrateEfficient f a b = integ f a b (f a) (f b)
 integ f a b fa fb = ((fa+fb)*(b-a)/2):
                         (map addpair (zip (integ f a m fa fm)
@@ -53,8 +66,8 @@ easydiff :: (Double -> Double) -> Double -> Double -> Double
 easydiff f x h = (f (x+h) - f x)/h
 
 --improved variation of easydiff, better approximation
-differentiate :: Double -> (Double -> Double) -> Double -> [Double]
-differentiate h0 f x = map (easydiff f x) (repeat' halve h0)
+differentiate :: Map -> XCoordinate -> InitialH -> [Slope]
+differentiate f x h0 = map (easydiff f x) (repeat halve h0)
 
 --calculates the next function value for the square root approximation
 next :: Double -> Double -> Double
@@ -63,24 +76,24 @@ next n x = (x + n/x)/2
 --by bringing together the generator and selector functions,
 --we can get an actual result. repeatedly, the selector checks for the termination
 --criteria and lets the generator calculate another value if needed.
-sqrt' :: Double -> Double -> Double -> Double
-sqrt' a0 eps n = within eps (repeat' (next n) a0)
+sqrt :: InitialApprox -> Epsilon -> SquareArg -> Approx
+sqrt a0 eps n = within eps (repeat (next n) a0)
 
 --(next n) a0 == next n a0, a -> a -> a == a -> (a -> a)
-relativesqrt :: Double -> Double -> Double -> Double
-relativesqrt a0 eps n = relative eps (repeat' (next n) a0)
+relativesqrt :: InitialApprox -> Epsilon -> SquareArg -> Approx
+relativesqrt a0 eps n = relative eps (repeat (next n) a0)
 
 --integration
-intgrt :: (Double -> Double) -> Double -> Double -> Double -> Double
+intgrt :: Map -> Low -> High -> Epsilon -> Area
 intgrt f a b eps = within eps (integrateNaive f a b)
 
-relativeintgrt :: (Double -> Double) -> Double -> Double -> Double -> Double
+relativeintgrt :: Map -> Low -> High -> Epsilon -> Area
 relativeintgrt f a b eps = relative eps (integrateNaive f a b)
 
-intgrtEff :: (Double -> Double) -> Double -> Double -> Double -> Double
+intgrtEff :: Map -> Low -> High -> Epsilon -> Area
 intgrtEff f a b eps = within eps (integrateEfficient f a b)
 
-relativeintgrtEff :: (Double -> Double) -> Double -> Double -> Double -> Double
+relativeintgrtEff :: Map -> Low -> High -> Epsilon -> Area
 relativeintgrtEff f a b eps = relative eps (integrateEfficient f a b)
 
 --helpfunction for testing the integral/differential approximations
@@ -89,8 +102,8 @@ testF :: (Num a) => a -> a
 testF x = x^2
 
 --differentiation
-diff :: Double -> (Double -> Double) -> Double -> Double -> Double
-diff h0 f x eps = within eps (differentiate h0 f x)
+diff :: Map -> XCoordinate -> InitialH -> Epsilon -> Slope
+diff f x h0 eps = within eps (differentiate f x h0)
 
-relativediff :: Double -> (Double -> Double) -> Double -> Double -> Double
-relativediff h0 f x eps = relative eps (differentiate h0 f x)
+relativediff :: Map -> XCoordinate -> InitialH -> Epsilon -> Slope
+relativediff f x h0 eps = relative eps (differentiate f x h0)
