085916970d9269733552b5bad5c9c4573625a425
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-03-27 01:07:29 +0200

    working dart_ts, even terminates :P

---
+++ UE2/lu2_christoph.hs
@@ -0,0 +1,65 @@
+import Data.List
+
+type Low = Double
+type High = Double
+type Approx = [Double]
+
+type Points = Int -- Punktwert einer Dart-Scheibe; echt positive Zahl
+type Dartboard = [Points] -- Dart-Scheibe charakterisiert durch Liste echt aufsteigender Punktwerte
+type Turn = [Points] -- Punktwerte einer Wurffolge; nur Punktwerte, die auf der Scheibe vorkommen
+-- (d.h., im Dartboard-Wert vorkommen) sind moeglich, auch mehrfach moeglich.
+type Turns = [Turn] -- Strom von Wurffolgen
+type TargetScore = Int -- Gewuenschte Zielpunktsumme > 0
+type Throws = Int -- Anzahl von Wuerfen einer Wurffolge > 0
+
+--maxDeviation1 :: Low -> High -> Approx
+
+--powDAC :: Integer -> Integer
+--powDAC = divideAndConquer...
+
+--maxDeviation2 :: Low -> High -> Approx
+
+--powMemo :: Integer -> Integer
+--powMemo 0 = ...
+--powMemo t = ...
+
+--gen_turns :: Dartboard -> Turns
+gen_turns :: Dartboard -> Turns
+gen_turns d = concat (map (gen_turns' d) [1..])
+
+gen_turns' :: Dartboard -> Throws -> Turns
+gen_turns' a b = nub (map sort (mapM (const a) [1 .. b]))
+
+--filters the turns that points sum up to the right value
+filter_turns_ts :: Turns -> TargetScore -> Turns
+filter_turns_ts as ts = filter (\x -> sum x == ts) as
+
+--filters the turns that have the right amount of throws
+filter_turns_th :: Turns -> Throws -> Turns
+filter_turns_th as th = filter (\x -> length x == th) as
+
+--selects the turns with the minimum length
+select_turns_minl :: Turns -> Turns
+select_turns_minl as = filter (\x -> length x == m) as where m = minLen as
+
+minLen :: [[a]] -> Int
+minLen as = minimum (map (\x -> length x) as)
+
+--transforms the turns to sorted lists
+--this is already given by our implementation of gen_turns
+transf_sort_turns :: Turns -> Turns
+transf_sort_turns as = as
+
+--stops gen_turns when the number of throws in a turn > TargetScore
+ts_stop :: TargetScore -> Turns -> Turns -> Int -> Turns
+ts_stop ts (a:as) turns i0
+ | length a <= ts = ts_stop ts as turns (i0+1)
+ | otherwise = take i0 turns
+
+-- returns the turns with the right value of points
+dart_ts :: Dartboard -> TargetScore -> Turns
+dart_ts d ts = filter_turns_ts (ts_stop ts (gen_turns d) (gen_turns d) 0) ts
+
+--dart_tst :: Dartboard -> TargetScore -> Throws -> Turns
+
+--dart_tsml :: Dartboard -> TargetScore -> Turns
--- UE2/test.hs
+++ UE2/test.hs
@@ -7,8 +7,14 @@ type Turns       = [Turn]   -- Strom von Wurffolgen
 type TargetScore = Int      -- Gewuenschte Zielpunktsumme > 0
 type Throws      = Int      -- Anz. von Wuerfen einer Wurffolge > 0
 
---executes gen_single for all sub list lengths (e.g. [1,2,3] -> [1], [1,2], [1,2,3]
-gen_all :: (Ord a, Eq a) => [a] -> [[a]] -> [[a]] -> Int -> Int -> [[a]]
+-- super ugly version.. this calculates way too much stuff that just gets filtered out anyways.
+-- TODO: omit gen_all, modify gen single? 
+-- FACT: [1,2,3] => [[1],[2],[3],[1,1],..,[1],[2],[3],..] lower parts are re-calculated in every iteration!
+gen_turns :: Dartboard -> Turns
+gen_turns d = removeDupl (concat (map (gen_all d [] [] 1) [1..]))
+
+-- executes gen_single for all sub list lengths (e.g. [1,2,3] -> [1], [1,2], [1,2,3]
+gen_all :: Dartboard -> Turns -> Turns -> Int -> Int -> Turns
 gen_all as bs ret i0 iN
  | bs == [] = gen_all as startList (ret ++ startList) 1 iN
  | i0 < iN = gen_all as (gen_single as bs [] 0) (ret ++ (gen_single as bs [] 0)) (i0+1) iN
@@ -17,7 +23,7 @@ gen_all as bs ret i0 iN
 
 -- adds one element of the dartboard to all the lists
 -- for all elements in the dartboard
-gen_single :: (Ord a) => [a] -> [[a]] -> [[a]] -> Int -> [[a]]
+gen_single :: Dartboard -> Turns -> Turns -> Int -> Turns
 gen_single as bs ret i0
  | i0 < length as = gen_single as bs (ret ++ (map (addElem (as!!i0)) bs)) (i0+1)
  | otherwise = removeDupl ret
@@ -25,17 +31,17 @@ gen_single as bs ret i0
 makeList :: a -> [a]
 makeList a = [a]
 
---hilfsfunktion mappt a aus as zu jedem element in bs
+-- hilfsfunktion mappt a aus as zu jedem element in bs
 addElem :: a -> [a] -> [a]
 addElem a bs = a:bs
 
---find duplicate tuples
+-- find duplicate tuples
 duplList :: (Ord a) => [a] -> [a] -> Bool
 duplList as bs
  | sort as == sort bs = True
  | otherwise = False
 
---remove duplicates from list
+-- remove duplicates from list
 removeDupl :: (Ord a) => [[a]] -> [[a]]
 removeDupl [] = []
 removeDupl [x] = [x]
