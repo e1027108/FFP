c0f649a79972628a82993b7f2a4e7405d17929fc
patrick.bellositz <patrick.bellositz@192.168.2.4>
2017-03-30 12:34:08 +0200

    created folder, file

---
+++ UE3/lu3.hs
@@ -0,0 +1,43 @@
+type Points = Int -- Punktwert einer Dart-Scheibe; echt positive Zahl
+type Dartboard = [Points] -- Dart-Scheibe charakterisiert durch Liste echt aufsteigender Punktwerte
+type Turn = [Points] {- Punktwerte einer Wurffolge; nur Punktwerte, die auf der Scheibe vorkommen
+(d.h., im Dartboard-Wert vorkommen) sind moeglich, auch mehrfach moeglich. -}
+type Turns = [Turn] -- Strom von Wurffolgen
+type TargetScore = Int -- Gewuenschte Zielpunktsumme > 0
+type Throws = Int -- Anzahl von Wuerfen einer Wurffolge > 0
+
+--data Node = ...
+
+--bt_dart_ts :: Dartboard -> TargetScore -> Turns
+
+--succ_ts :: Node -> [Node]
+--goal_ts :: Node -> Bool
+
+--bt_dart_tst :: Dartboard -> TargetScore -> Throws -> Turns
+
+--succ_tst :: Node -> [Node]
+--goal_tst :: Node -> Bool
+
+--bt_dart_tsml :: Dartboard -> TargetScore -> Turns
+
+--succ_tsml :: Node -> [Node]
+--goal_tsml :: Node -> Bool
+
+--searchDfs :: (Eq node) => (node -> [node]) -> (node -> Bool) -> node -> [node]
+
+--succ :: node -> [node]
+--goal :: node -> Bool
+
+
+--searchPfsFst :: (Ord node) => (node -> [node]) -> (node -> Bool) -> node -> [node]
+
+
+--psf_low :: Dartboard -> Targetscore -> Turns
+
+--psf_high :: Dartboard -> Targetscore -> Turns
+
+--seems a bit redundant?
+--succ_low :: Node -> [Node]
+--goal_high :: Node -> Bool
+--succ_low :: Node -> [Node]
+--goal_high :: Node -> Bool
