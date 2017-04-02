3b3c5cbc2bb44b5e38966cea8701a00082e7a0f8
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-04-02 17:28:07 +0200

    seems to work, need to add filter for duplicates

--- UE3/lu3_christoph.hs
+++ UE3/lu3_christoph.hs
@@ -24,29 +24,37 @@ type Turns       = [Turn]   -- Strom von Wurffolgen
 type TargetScore = Int      -- Gewuenschte Zielpunktsumme > 0
 type Throws      = Int      -- Anzahl von Wuerfen einer Wurffolge > 0
 
---bt_dart_ts :: Dartboard -> TargetScore -> Turns
---bt_dart_ts d ts
+--The type Node carries information on the Turn (including this nodes Points),
+--the TargetScore and the right number of Throws. Also, a list of successors is given
+data Node = Nil | N Dartboard Turn TargetScore Throws deriving Show
 
-data Node = Nil | N Points [Node] deriving Show
-
+--Equality for type Node, made up of individual (==) calls on all the components of Node
 instance Eq Node where
- N x xs == N y ys = x == y && xs == ys 
+ N d1 t1 ts1 th1 == N d2 t2 ts2 th2 = 
+  d1 == d2 && t1 == t2 && ts1 == ts2 && th1 == th2
  Nil == Nil = True
  Nil == _ = False
 
---searchDfs :: (Eq Node) => (Node -> [Node]) -> (Node -> Bool) -> Node -> [Node]
---searchDfs succ goal x 
--- = (search' (push x emptyStack))
--- where search' s
---        | stackEmpty s = []
---        | goal (top s) = top s : search' (pop s)
---        | otherwise = let x = top s in search' (foldr push (pop s) (succ x))
+searchDfs :: (Node -> [Node]) -> (Node -> Bool) -> Node -> [Node]
+searchDfs succ goal x 
+ = (search' (push x emptyStack))
+ where search' s
+        | stackEmpty s = []
+        | goal (top s) = top s : search' (pop s)
+        | otherwise = let x = top s in search' (foldr push (pop s) (succ x))
 
 succ_ts :: Node -> [Node]
--- succ Nil = error "successor on empty node"
 succ_ts Nil = [] 
-succ_ts (N x xs) = xs
+succ_ts (N d t ts th) = [(N d (x:t) ts th)| x <- d , sum (x:t) <= ts]
 
---goal :: node -> Bool
---goal Nil = False
---goal x = 
+goal_ts :: Node -> Bool
+goal_ts Nil = False
+goal_ts (N d t ts th)
+ | sum t == ts = True
+ | otherwise   = False
+
+bt_dart_ts :: Dartboard -> TargetScore -> Turns
+bt_dart_ts d ts = concat [map getTurn (searchDfs succ_ts goal_ts (N d [x] ts 0))| x <- (filter (<=ts) d)]
+
+getTurn :: Node -> Turn
+getTurn (N d t ts th) = t
