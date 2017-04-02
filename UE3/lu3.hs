1748e7ead48858f3af0e222c083718d6fec6cb77
patrick.bellositz <patrick.bellositz@192.168.2.4>
2017-04-02 21:16:00 +0200

    renamed file, deleted old file

--- UE3/lu3.hs
+++ UE3/lu3.hs
@@ -1,97 +1,117 @@
-module Stack (Stack,push,pop,top,emptyStack,stackEmpty) where 
+import Stack
 import Data.List
 
-push       :: a -> Stack a -> Stack a
-pop        :: Stack a -> Stack a
-top        :: Stack a -> a
-emptyStack :: Stack a
+type Points      = Int      -- Punktwert einer Dart-Scheibe; echt positive Zahl
+type Dartboard   = [Points] -- Dart-Scheibe charakterisiert durch Liste echt aufsteigender Punktwerte
+type Turn        = [Points] -- Punktwerte einer Wurffolge; nur Punktwerte, die auf der Scheibe vorkommen
+                            -- (d.h., im Dartboard-Wert vorkommen) sind moeglich, auch mehrfach moeglich
+type Turns       = [Turn]   -- Strom von Wurffolgen
+type TargetScore = Int      -- Gewuenschte Zielpunktsumme > 0
+type Throws      = Int      -- Anzahl von Wuerfen einer Wurffolge > 0
 
-data Stack a = EmptyStk | Stk a (Stack a)
+--The type Node carries information on the Turn (including this nodes Points),
+--the TargetScore and the right number of Throws. Also, a list of successors is given
+data Node = Nil | N Dartboard Turn TargetScore Throws deriving Show
 
-push x s            = Stk x s
-pop EmptyStk        = error "pop from empty stack"
-pop (Stk _ s)       = s
-top EmptyStk        = error "top from empty stack"
-top (Stk x _)       = x
-emptyStack          = EmptyStk
-stackEmpty EmptyStk = True
-stackEmpty _        = False
-
-type Points = Int -- Punktwert einer Dart-Scheibe; echt positive Zahl
-type Dartboard = [Points] -- Dart-Scheibe charakterisiert durch Liste echt aufsteigender Punktwerte
-type Turn = [Points] {- Punktwerte einer Wurffolge; nur Punktwerte, die auf der Scheibe vorkommen
-(d.h., im Dartboard-Wert vorkommen) sind moeglich, auch mehrfach moeglich. -}
-type Turns = [Turn] -- Strom von Wurffolgen
-type TargetScore = Int -- Gewuenschte Zielpunktsumme > 0
-type Throws = Int -- Anzahl von Wuerfen einer Wurffolge > 0
-
---is this allowed? we'll see
-data Node = Empty | N Dartboard Turn Throws TargetScore deriving Show
-
---bt_dart_ts :: Dartboard -> TargetScore -> Turns
-
---succ_ts :: Node -> [Node]
---goal_ts :: Node -> Bool
-
---computes all Turns of specified target score in specified amount of throws
-bt_dart_tst :: Dartboard -> TargetScore -> Throws -> Turns
-bt_dart_tst d ts th = concat [map getTurn (searchDfs succ_tst goal_tst (N d [x] th ts)) | x <- (sort d) ]
+--Equality for type Node, made up of individual (==) calls on all the components of Node
+instance Eq Node where
+ N d1 t1 ts1 th1 == N d2 t2 ts2 th2 = 
+  d1 == d2 && t1 == t2 && ts1 == ts2 && th1 == th2
+ Nil == Nil = True
+ Nil == _ = False
 
 searchDfs :: (Node -> [Node]) -> (Node -> Bool) -> Node -> [Node]
-searchDfs succ goal x = (search' (push x emptyStack) )
-    where
-        search' s
-            | stackEmpty s = []
-            | goal (top s) = top s : search' (pop s)
-            | otherwise = let x = top s
-                in search' (foldr push (pop s) (succ x))
+searchDfs succ goal x 
+ = (search' (push x emptyStack))
+ where search' s
+        | stackEmpty s = []
+        | goal (top s) = top s : search' (pop s)
+        | otherwise = let x = top s in search' (foldr push (pop s) (succ x))
 
-{-
-generates all turns that are extensions to the input node and returns a list of them
-only generates nodes if input node permits adding another throw, throws are added in a sorted fashion
--}
+-- ****************************************************************************
+-- succ and goal functions to support the solutions of the 3 different problems
+-- ****************************************************************************
+
+--computes the successors while cutting off the values that would,
+--in sum with the remaining turn, exceed the target score
+succ_ts :: Node -> [Node]
+succ_ts Nil = [] 
+succ_ts (N d t ts th) = [(N d (x:t) ts th)| x <- d , sum (x:t) <= ts]
+
+--checks if a node has a summed up value equal to the target score
+goal_ts :: Node -> Bool
+goal_ts Nil = False
+goal_ts (N d t ts th)
+ | sum t == ts = True
+ | otherwise   = False
+
+--computes all turns that are extensions to the input node 
+--(while not exceeding the number of throws) and returns a list 
 succ_tst :: Node -> [Node]
-succ_tst Empty = []
+succ_tst Nil = []
 succ_tst (N d turn th ts)
     | th > (length turn) = [ (N d x th ts) | x <- (generate turn d) ]
     | otherwise = [] --we want to stop here
 
-{-
-adds to list of throws another throw from given dartboard
-does not consider throws that are smaller than the highest one for purposes of sorting
--}
+--help function, adds another throw if possible
 generate :: Turn -> Dartboard -> Turns
 generate t d 
     | t /= [] = [ t ++ [x] | x <- (filter (>= (maximum t)) d) ]
     | otherwise = [ [x] | x <- d ]
 
--- checks for the node whether throws are of specified amount and score is of specified sum
+--checks for the node whether throws are of specified amount and score is of specified sum
 goal_tst :: Node -> Bool
-goal_tst Empty = False
+goal_tst Nil = False
 goal_tst (N d turn th ts)
     | (length turn) == th && (sum turn) == ts = True
     | otherwise = False
-    
+
+--computes successors filtering duplicates in range of given targetscore
+succ_tsml :: Node -> [Node]
+succ_tsml Nil = []
+succ_tsml (N d t th ts) = [(N d (x:t) th ts)| x <- (filter (>= (maximum t)) d) , sum (x:t) <= ts]
+
+--same as goal_ts
+goal_tsml :: Node -> Bool
+goal_tsml Nil = False
+goal_tsml (N d turn th ts)
+    | (sum turn) == ts = True
+    | otherwise = False
+
+-- *************
+--Help functions
+-- *************
+
+--removes duplicate entries from a list of turns
+removeDuplicates :: Turns -> Turns
+removeDuplicates [] = []
+removeDuplicates (t1:turns)
+ | elem t1 turns = removeDuplicates turns
+ | otherwise     = t1 : removeDuplicates turns
+
+--returns the turn of a node
 getTurn :: Node -> Turn
 getTurn (N d t ts th) = t
 
---bt_dart_tsml :: Dartboard -> TargetScore -> Turns
+--returns list with minimum length entries
+minlength :: Turns -> Turns
+minlength [] = []
+minlength turns = filter (\x -> length x <= minLen turns) turns 
 
---succ_tsml :: Node -> [Node]
---goal_tsml :: Node -> Bool
+minLen :: Turns -> Throws
+minLen [] = 0
+minLen as = minimum (map (\x -> length x) as)
 
---sort :: Turn -> Turn
+-- ************************************************************
+--functions to answer the proposed questions using backtracking
+-- ************************************************************
+--computes all Turns of specified target score
+bt_dart_ts :: Dartboard -> TargetScore -> Turns
+bt_dart_ts d ts = removeDuplicates (map sort (concat [map getTurn (searchDfs succ_ts goal_ts (N d [x] ts 0))| x <- (filter (<=ts) d)]))
 
+--computes all Turns of specified target score in specified amount of throws
+bt_dart_tst :: Dartboard -> TargetScore -> Throws -> Turns
+bt_dart_tst d ts th = concat [map getTurn (searchDfs succ_tst goal_tst (N d [x] th ts)) | x <- (sort d) ]
 
---searchPfsFst :: (Ord node) => (node -> [node]) -> (node -> Bool) -> node -> [node]
-
-
---psf_low :: Dartboard -> Targetscore -> Turns
-
---psf_high :: Dartboard -> Targetscore -> Turns
-
---seems a bit redundant?
---succ_low :: Node -> [Node]
---goal_high :: Node -> Bool
---succ_low :: Node -> [Node]
---goal_high :: Node -> Bool
+bt_dart_tsml :: Dartboard -> TargetScore -> Turns
+bt_dart_tsml d ts = minlength (concat [map getTurn (searchDfs succ_tsml goal_tsml (N d [x] 0 ts)) | x <- (filter (<= ts) d)])
