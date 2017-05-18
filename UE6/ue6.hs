1c12def76e015b739032b93ac7497b417d1bff06
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-05-18 19:34:45 +0200

    methode 1 und 2 funktionieren mMn, der punkt "mit QuickCheck testen" fehlt noch

---
+++ UE6/ue6.hs
@@ -0,0 +1,43 @@
+import Prelude hiding (Word)
+import Data.Maybe
+import Data.List
+
+type Text  = String
+type Word  = String
+type First = Int
+type Last  = Int
+
+occS :: Text -> Word -> [(First,Last)]
+occS text word = getOcc skipS (length(word)-1) 0 text word
+
+occI :: Text -> Word -> [(First,Last)]
+occI text word = getOcc skipI (length(word)-1) 0 text word
+
+{- added a parameter for skip width
+    (occS: 1,
+     occI: calculated via index of last occurrence in word (length(word) - 1 - index)
+             (can this be done without recalculating the values every time? e.g. Memoization?)
+-}
+getOcc :: (Word -> Char -> Int) -> Int -> Int -> Text -> Word -> [(First,Last)]
+getOcc _ _ _ "" _ = error "string can't be empty"
+getOcc _ _ _ _ "" = error "string can't be empty"
+getOcc skip tpos wcnt text word
+ | tpos >= length(text)   = []                                                    -- no more occurrences possible
+ | wcnt == length(word)-1 = if checkSame then [(tpos-wcnt,tpos)] ++ getOcc skip (tpos+calcSkip) 0 text word
+                              else getOcc skip (tpos+calcSkip) 0 text word        -- found a word, add start/end-index and carry on
+ | otherwise              = if checkSame then getOcc skip tpos (wcnt+1) text word -- keep matching single chars until mismatch or word end
+                              else getOcc skip (tpos+calcSkip) 0 text word
+ where checkSame = (word!!(length(word)-1-wcnt) == text!!(tpos-wcnt))
+       calcSkip  = (skip word (text!!(tpos-wcnt)))
+
+-- simple skip width
+skipS :: Word -> Char -> Int
+skipS _ _ = 1
+
+{- improved skip width
+   - This function takes the last occurence of a char in the search pattern "word"
+   and returns the index as a safe skip width.
+   - Maybe can be "Nothing" or "Just "value"", fromMaybe defines a default case for "Nothing"
+-}
+skipI :: Word -> Char -> Int
+skipI w c = (fromMaybe (length(w)-1) (elemIndex c (reverse w)))
