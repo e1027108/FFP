436016dc878aa994340320327ba8aa2d039fce72
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-06-18 15:59:33 +0200

    stream und diag evaluation strategies seem to work

--- UE8/ue8.hs
+++ UE8/ue8.hs
@@ -1,14 +1,58 @@
+import Control.Monad
+import Control.Applicative
+
+type Stream a = [a]
+
 class Monad m => Bunch m where
  zero :: m a
  alt  :: m a -> m a -> m a
  wrap :: m a -> m a
 
-data Term = Int Int
-            | Nil
-            | Cons Term Term
-            | Var Variable deriving Eq
+instance Bunch [] where
+ zero      = []
+ alt xs ys = xs ++ ys
+ wrap xs   = xs
 
+newtype Diag a = MkDiag (Stream a) deriving Show
+
+unDiag :: Diag a -> Stream a
+unDiag (MkDiag xs) = xs
+
+diag :: Stream (Stream a) -> Stream [a]
+diag [] = []
+diag (xs:xss) = lzw (++) [ [x] | x <- xs] ([] : diag xss)
+
+instance Monad Diag where
+ return x = MkDiag [x]
+ MkDiag xs >>= f = MkDiag (concat (diag (map (unDiag . f) xs)))
+
+-- for ghci compatibility --
+instance Applicative Diag where
+ pure  = return
+ (<*>) = ap
+
+instance Functor Diag where
+ fmap = liftM
+----------------------------
+
+instance Bunch Diag where
+ zero    = MkDiag []
+ alt (MkDiag xs) (MkDiag ys)
+         = MkDiag (shuffle xs ys)
+ wrap xm = xm
+
+shuffle :: [a] -> [a] -> [a]
+shuffle [] ys = ys
+shuffle (x:xs) ys = x : shuffle ys xs
+
+data Term = Int Int | Nil | Cons Term Term | Var Variable deriving (Show, Eq)
 data Variable = Named String | Generated Int deriving (Show, Eq)
+
+var :: String -> Term
+var s = Var (Named s)
+
+list :: [Int] -> Term
+list xs = foldr Cons Nil (map Int xs)
 
 (=:=) :: Bunch m => Term -> Term -> Pred m
 (t=:=u)(MkAnswer(s,n)) =
@@ -26,7 +70,8 @@ infixr 4 =:=
 infixr 3 &&&
 infixr 2 |||
 
-newtype Subst = MkSubst [(Var, Term)]
+-- kann man [(Variable,Term)] statt [(Var,Term)] sagen?
+newtype Subst = MkSubst [(Variable, Term)] deriving Show
 unSubst(MkSubst s) = s
 
 idsubst = MkSubst[]
@@ -82,5 +127,14 @@ exists p (MkAnswer (s,n)) =
 
 type Pred m = Answer -> m Answer
 
-
 newtype Answer = MkAnswer (Subst, Int) deriving Show
+
+-- supporting function lzw (like zip with - except non-empty part of a list gets attached) --
+lzw :: (a -> a -> a) -> Stream a -> Stream a -> Stream a
+lzw f [] ys         = ys
+lzw f xs []         = xs
+lzw f (x:xs) (y:ys) = (f x y):(lzw f xs ys)
+
+-- supporting function test --
+test :: Bunch m => Bool -> m ()
+test b = if b then return () else zero
